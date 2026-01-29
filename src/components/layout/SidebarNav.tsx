58fe656c398ba0b7342f6407cdb2b9abd8f8f089
Agents Architects <architects@agentsarchitects.ai>
2026-01-29 17:43:34 +0530

    Client portal updated

--- src/components/layout/SidebarNav.tsx
+++ src/components/layout/SidebarNav.tsx
@@ -53,7 +53,7 @@ const SidebarNav: React.FC<SidebarNavProps> = ({ collapsed }) => {
     {
       section: 'DENPAY',
       items: [
-        { key: '/denpay', icon: <DollarOutlined />, label: 'Go To Client Portal', external: true, externalUrl: 'api-uat-uk-workfin-03.azurewebsites.net' }
+        { key: '/denpay', icon: <DollarOutlined />, label: 'Go To Client Portal', external: true, externalUrl: 'https://api-uat-uk-workfin-03.azurewebsites.net' }
       ]
     },
     {
